a5fb091bf3aa336466f0c414f28888b2fd08e090
Shulammite Odde <shullyd7@gmail.com>
2022-07-26 01:05:48 +0100

    Added my project

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'i9redurqo8';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-thvlkrh0.us.auth0.com',
+  clientId: 'G9G70WC9vmZ1JN5XKhTSwxFd0Mtf5HPT',
+  callbackUrl: 'http://localhost:3000/callback'
+};
